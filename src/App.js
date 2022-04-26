c0a419ae14789792b7e6ea867608b9ca6575bcc9
Déborah <deborahclerckx@gmail.com>
2022-04-26 03:29:50 +0200

    effect hook, fetch data, select le mot d'un objt au hasard, affiche la solution +comment error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [solution, setSolution] = useState(null)
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions') // erreur: oui... un url sans '://'.... tout va bien... :o)
+      .then(res => res.json())
+      .then(json => {
+        // random int entre 0 & 14
+        const randomSolution = json[Math.floor(Math.random()*json.length)]
+        setSolution(randomSolution.word) // erreur: important de mettre .word car la solution est un objet. Ce qui nous intéresse est le mot de cet objet, non pas l'objet lui-même.
+      })
+  }, [setSolution])
+
   return (
     <div className="App">
-      <h1>Wordle Clone</h1>
+      <h1>Wordle (Lingo)</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// erreur principale: il est important de laisser le terminal ouvert pour le json-server...... :o)
