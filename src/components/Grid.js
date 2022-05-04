34f84005d1c4304ab39df247b5b6a498f679d9f2
Déborah <deborahclerckx@gmail.com>
2022-05-04 15:32:54 +0200

    afficher les mots essayés dans la grille, appliquer la bonne couleur, fonctionnel + modif css & readme

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,8 +4,8 @@ import Row from './Row'
 export default function Grid({ currentGuess, guesses, turn }) { // erreur: j'avais oublié les { } pour englober currentGuess, guesses, turn
     return (
         <div>
-            {guesses.map((g, i) => {
-                return <Row key={i}/>
+            {guesses.map((g, i) => { // g = guesses, i = index
+                return <Row key={i} guess={g} />
             })}
         </div>
     )
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
-export default function Row() {
-    
+export default function Row({ guess }) {
+
+    // check value guess
+    if (guess) {
+        return (
+            // erreur: j'avais écris class="" au lieu de className=""
+            <div className="row past">
+                {guess.map((l, i) => ( // l = letters, i = index
+                    <div key={i} className={l.color}>{l.key}</div>
+                ))}
+            </div>
+        )
+    }
+
     return (
         <div className='row'>
             <div></div>
